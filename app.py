b91066a88917b87140a23ba44e0f2b29fcb760d6
petermac <peter0428@vip.qq.com>
2018-09-07 12:42:39 +0800

    前端传递json

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import json
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.ini')
 
-@app.route('/test/<a>/<b>/<c>/<d>/<e>', methods=['POST'])
-def test(a, b, c, d, e):
-    json_dict = {
-        "A": a,
-        "B": b,
-        "C": c,
-        "D": d,
-        "E": e
-    }
-    return jsonify(json_dict)
+@app.route('/test', methods=['POST'])
+def test():
+    #接收前端发来的数据data
+    data = json.loads(request.get_data())
+    A = data["a"]
+    B = data["b"]
+    C = data["c"]
+    D = data["d"]
+    E = data["e"]
+
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
